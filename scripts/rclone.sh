e8979924464bb853169c6c349ae08d33a60dd9dd
Phil_Goud <phil@goud.so>
2024-05-25 16:27:58 +0200

    Update rclone.sh

--- scripts/rclone.sh
+++ scripts/rclone.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
 #backups et ajouts au fichier de log rclone.txt
-echo "📥 BACKUP RCLONE 🆗" &> /DATA/log/rclonelog.txt
+echo "📦 BACKUP RCLONE 🆗" &> /DATA/log/rclonelog.txt
 
-#Dossier /Famille
-sudo rclone sync -v -c --check-first /mnt/CAKE/Famille/ swissbackup:/Famille/ &> /DATA/log/rclonelogfamille.txt
+
+echo "Dossier /Famille" &> /DATA/log/rclonelogtemp.txt
+sudo rclone sync -v -c --check-first /mnt/CAKE/Famille/ swissbackup:/Famille/ &>> /DATA/log/rclonelogtemp.txt
 echo "📽️ Famille ✔️" &>> /DATA/log/rclonelog.txt
 echo $(date +"%d-%m-%y %H:%M:%S") &>> /DATA/log/rclonelog.txt
 #Ici on récupère la dernière itération des infos Checks, Transferred et Elapsed
-grep -e '^Checks' /DATA/log/rclonelogfamille.txt | tail -1 &>> /DATA/log/rclonelog.txt 
-grep -e '^Transferred' /DATA/log/rclonelogfamille.txt | tail -1 &>> /DATA/log/rclonelog.txt
-grep -e '^Elapsed' /DATA/log/rclonelogfamille.txt | tail -1 &>> /DATA/log/rclonelog.txt
+grep -e '^Checks' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt 
+grep -e '^Transferred' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt
+grep -e '^Elapsed' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt
 
-#Dossier /ASoundMR
-sudo rclone sync -v -c --check-first /mnt/CAKE/ASoundMR/ swissbackup:/ASoundMR/ &> /DATA/log/rclonelogasoundmr.txt
+echo "Dossier /ASoundMR" &> /DATA/log/rclonelogtemp.txt
+sudo rclone sync -v -c --check-first /mnt/CAKE/ASoundMR/ swissbackup:/ASoundMR/ &>> /DATA/log/rclonelogtemp.txt
 echo "🪶 ASoundMR ✔️" &>> /DATA/log/rclonelog.txt
 echo $(date +"%d-%m-%y %H:%M:%S") &>> /DATA/log/rclonelog.txt
-grep -e '^Checks' /DATA/log/rclonelogasoundmr.txt | tail -1 &>> /DATA/log/rclonelog.txt 
-grep -e '^Transferred' /DATA/log/rclonelogasoundmr.txt | tail -1 &>> /DATA/log/rclonelog.txt
-grep -e '^Elapsed' /DATA/log/rclonelogasoundmr.txt | tail -1 &>> /DATA/log/rclonelog.txt
+grep -e '^Checks' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt 
+grep -e '^Transferred' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt
+grep -e '^Elapsed' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt
 
-#Dossier /scripts
-sudo rclone sync -v -c --check-first /scripts/ swissbackup:/scripts/ &> /DATA/log/rclonelogscripts.txt
+echo "Dossier /scripts" &> /DATA/log/rclonelogtemp.txt
+sudo rclone sync -v -c --check-first /scripts/ swissbackup:/scripts/ &>> /DATA/log/rclonelogtemp.txt
 echo "🛠️ scripts ✔️" &>> /DATA/log/rclonelog.txt
 echo $(date +"%d-%m-%y %H:%M:%S") &>> /DATA/log/rclonelog.txt
-grep -e '^Checks' /DATA/log/rclonelogscripts.txt | tail -1 &>> /DATA/log/rclonelog.txt 
-grep -e '^Transferred' /DATA/log/rclonelogscripts.txt | tail -1 &>> /DATA/log/rclonelog.txt 
-grep -e '^Elapsed' /DATA/log/rclonelogscripts.txt | tail -1 &>> /DATA/log/rclonelog.txt
+grep -e '^Checks' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt 
+grep -e '^Transferred' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt 
+grep -e '^Elapsed' /DATA/log/rclonelogtemp.txt | tail -1 &>> /DATA/log/rclonelog.txt
 
 #La petite config telegram
 #on récupère le contenu du rclonelog
 TELEGRAM=`cat /DATA/log/rclonelog.txt`
 #les identifiants nécéssaires à l'envoi du message
-TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-CHAT_ID="XXXXXXXXXX"
+TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXX"
+CHAT_ID="XXXXXXXX"
 
 #Verification du nombre de caractères (limite de 1024 sur Telegram)
 LENGTH=${#TELEGRAM}
@@ -45,7 +46,7 @@ if (($LENGTH < 1000)); then
     exit
 else
 #Telegram notif 2 si message trop gros
-        curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="📥 BACKUP RCLONE🆗
+        curl -s -X POST https://api.telegram.org/bot$TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="📦 BACKUP RCLONE🆗
         📽️ Famille ✔️
         🪶 ASoundMR ✔️
         🛠️ scripts ✔️
@@ -53,5 +54,3 @@ else
         exit
 fi
 done
-
-
